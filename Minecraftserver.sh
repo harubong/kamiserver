65af8328a3ce67b72d3d75beb0b0b162d04b14b7
kamiserver <121999122+kamiserver@users.noreply.github.com>
2023-03-11 20:35:12 +0900

    Update Minecraftserver.sh

--- Minecraftserver.sh
+++ Minecraftserver.sh
@@ -1,2 +1,2 @@
-curl https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.63.01.zip bash
+curl -o https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.63.01.zip
 unzip bedrock-server-1.19.63.01.zip 
