8a4be37397f8f4f985d10b330ea0bdd9ad0f5fe0
kamiserver <121999122+kamiserver@users.noreply.github.com>
2023-03-11 23:34:04 +0900

    Update server-start.sh

--- server-start.sh
+++ server-start.sh
@@ -1,1 +1,6 @@
-curl -
+#サーバープロパティを読み込んで起動する
+rm server.properties
+curl -O https://raw.githubusercontent.com/kamiserver/kamiserver/update/kami1234/server.properties
+LD_LIBRARY_PATH=. ./bedrock_server
+#サーバーの停止
+#stop
